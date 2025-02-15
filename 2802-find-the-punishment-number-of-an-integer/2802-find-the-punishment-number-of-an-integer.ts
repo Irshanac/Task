fa99151de95967633eb7dc7cd944a63b5ef6fe32
Irshana c <109659310+Irshanac@users.noreply.github.com>
2025-02-15 13:58:55 +0430

    Time: 35 ms (66.67%), Space: 57.6 MB (33.33%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.ts
@@ -0,0 +1,12 @@
+function punishmentNumber(end: number) {
+    const check = (n: number, tar: number): boolean => {
+        if (tar < 0 || n < tar || n === tar) return n === tar;
+        return [10, 100, 1000].some(
+            (div) => check(Math.floor(n / div), tar - n % div));
+    };
+    let res = 0;
+    for (let curr = 1, sqr = 1; curr <= end; sqr = ++curr ** 2) {
+        if (check(sqr, curr)) res += sqr;
+    }
+    return res;
+}
